49e9a9687983807cbfdf0a65fc0026fc86192d78
achmedhibatillah <ach.hibatillah@gmail.com>
2025-09-16 11:13:08 +0700

    update block mining

--- core/node/controllers/mining_execute.py
+++ core/node/controllers/mining_execute.py
@@ -4,10 +4,15 @@ import time
 
 from core.node.controllers.mempool_management import MempoolManagement
 from core.node.controllers.block_management import BlockManagement
+from core.node.controllers.state_management import StateManagement
 
 class ExecuteMining:
     def __init__(self):
         self.MEMPOOL_FILE = os.path.join(os.path.dirname(__file__), "..", "..", "..", "mempool", "mempool.json")
+
+        self.TARGET = "0001ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        self.obj_block_management = BlockManagement()
+        self.obj_state_management = BlockManagement()
 
     def mining(self):
         mempool_management = MempoolManagement()
@@ -19,8 +24,8 @@ class ExecuteMining:
 
         print("\nMining on proccess...")
 
-        block_management = BlockManagement()
-        blockchain = block_management.get_blockchain()
+        
+        blockchain = self.obj_block_management.get_blockchain()
         prev_hash = blockchain[-1]["hash"] if blockchain else "0"*64
         
         block = {
@@ -30,4 +35,13 @@ class ExecuteMining:
             "nc": 0,
             "tx": []
         }
+
+        print("\nMining on proccess...")
+
+        while True:
+            block["nc"] += 1
+            hash = self.obj_block_management.block_hash(block)
+            if hash < self.TARGET:
+                block["hs"] = hash
+                self.obj_block_management.block_save(newblock=block)
         
--- core/node/controllers/mining_txlist.py
+++ core/node/controllers/mining_txlist.py
@@ -4,13 +4,14 @@ from datetime import datetime
 
 class TxListMempool:
     def __init__(self):
-        self.MEMPOOL_FILE = os.path.join(os.path.dirname(__file__), "..", "..", "data", "mempool", "mempool.json")
+        self.MEMPOOL_FILE = os.path.join(os.path.dirname(__file__), "..", "..", "..", "mempool", "mempool.json")
 
     def index(self):
         with open(self.MEMPOOL_FILE, "r") as f:
             tx_mempool = json.load(f)
 
-        print(f"\nTransaction List")
+        count_tx = len(tx_mempool)
+        print(f"\nTransaction List ({count_tx})")
         print(f"{datetime.now()}\n")
         
         i = 1
